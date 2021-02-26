8ef3f4f755834459b2cc140d98ffd3c5faee4518
Jeroen Vester <jvester@sogyo.nl>
2021-02-26 10:24:30 +0100

    Implementation of Mancala game defined

--- domain/src/main/java/mancala/domain/MancalaImpl.java
+++ domain/src/main/java/mancala/domain/MancalaImpl.java
@@ -1,34 +1,73 @@
 package mancala.domain;
 
 public class MancalaImpl implements Mancala {
+    Kalaha kalaha_player_one;
+
     public MancalaImpl() {
-        // Initialize the game here.
+        this.kalaha_player_one = new Kalaha();
     }
 
     @Override
     public boolean isPlayersTurn(int player) {
-        return true;
+        if (player == Mancala.PLAYER_ONE) {
+            return this.kalaha_player_one.getPlayer().isCurrentPlayer();
+        } else {
+            return this.kalaha_player_one.getPlayer().getOpponent().isCurrentPlayer();
+        }
+    }
+
+    private Player getPlayer(int index) {
+        Player player;
+        if (index < 7) {
+            player = this.kalaha_player_one.getPlayer();
+        } else {
+            player = this.kalaha_player_one.getPlayer().getOpponent();
+        }
+        return player;
     }
 
     @Override
 	public void playPit(int index) throws MancalaException {
-        // Implement playing a pit.
+        Player player = getPlayer(index);
+        int id = (index + 1) % 7;
+        if (id == 0) {
+            this.kalaha_player_one.getKalaha(player).playPit();
+        } else {
+            this.kalaha_player_one.getPlayingPit(id, player).playPit();
+        }
     }
 	
 	@Override
 	public int getStonesForPit(int index) {
-        // Make a sane implementation.
-        if((index + 1 % 7) == 0) return 0;
-        return 4;
+        Player player = getPlayer(index);
+        int id = (index + 1) % 7;
+        if (id == 0) {
+            return this.kalaha_player_one.getKalaha(player).getStones();
+        } else {
+            return this.kalaha_player_one.getPlayingPit(id, player).getStones();
+        }
     }
 
 	@Override
 	public boolean isEndOfGame() {
-        return false;
+        return (!this.isPlayersTurn(Mancala.PLAYER_ONE) && !this.isPlayersTurn(Mancala.PLAYER_TWO)); 
     }
 
 	@Override
 	public int getWinner() {
-        return Mancala.NO_PLAYERS;
+        if (!this.isEndOfGame()) {
+            return Mancala.NO_PLAYERS;
+        } else {
+            int stones_player_one = this.kalaha_player_one.getStones();
+            Player player_two = this.kalaha_player_one.getPlayer().getOpponent();
+            int stones_player_two = this.kalaha_player_one.getKalaha(player_two).getStones();
+            if (stones_player_one > stones_player_two) {
+                return Mancala.PLAYER_ONE;
+            } else if (stones_player_one < stones_player_two) {
+                return Mancala.PLAYER_TWO;
+            } else {
+                return Mancala.BOTH_PLAYERS;
+            }
+        }
     }
 }
